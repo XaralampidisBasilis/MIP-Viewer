2065edf26a9138a565e5a98f0ccca6b231ca848e
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2026-04-02 12:05:49 +0300

    change gitignore

--- src/javascript/sources.js
+++ src/javascript/sources.js
@@ -5,11 +5,11 @@ export default [
         name: 'volume',
         type: 'niftiFile',
         // path: 'nifti/colon/volume.nii.gz',
-        path: 'nifti/head/CTA-Head-and-Neck.nii.gz',
+        // path: 'nifti/head/CTA-Head-and-Neck.nii.gz',
         // path: 'nifti/cardiac/mr_train_1014_image.nii.gz',
         // path: 'nifti/cardiac/ct_train_1002_image.nii.gz',
+        path: 'nifti/cardiac/MedHK23_CCA.nii.gz',
         // path: 'nifti/cardiac/CTACardio.nii.gz',
-        // path: 'nifti/cardiac/MedHK23_CCA.nii.gz',
         // path: 'nifti/cardiac/Angelou0516Cardiac.nii.gz',
     }, 
 ]
