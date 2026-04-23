3c099f42c8a755e837899e28dae6bc0140ae95c0
XaralampidisBasilis <xaralampidisbasilis@gmail.com>
2026-04-23 13:16:25 +0300

    minor

--- src/shaders/mip_viewer/chunks/shade/compute_shade.glsl
+++ src/shaders/mip_viewer/chunks/shade/compute_shade.glsl
@@ -3,6 +3,6 @@
 frag.color = colormap(u_shading.colormap, mip.value);
 
 // float depth = map(u_box.min_distance, u_box.max_distance, mip.distance);
-// frag.color = mix(frag.color, vec3(1.0 - depth), u_debug.variable2);
+// frag.color = mix(frag.color, 2.0 * vec3(1.0 - depth), u_debug.variable2);
 
 fragColor = vec4(frag.color, 1.0);
